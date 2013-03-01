05d51807f101bbb1eabce53dc4264d97fd4cf9b5
Christian N. Poveda-Ruiz <nicvsgodzilla@gmail.com>
2013-03-01 09:30:39 -0500

    least squares working

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -1,6 +1,6 @@
 import os
 import numpy
-
+import sys
 table = []
 
 for filename in os.listdir('hw4_data/' ):
@@ -9,15 +9,35 @@ for filename in os.listdir('hw4_data/' ):
 	
 	path = os.path.expanduser('hw4_data/' + filename)
 	
-	filein = open(path, 'r')
+	info0 = open(path, 'r').readlines()
 	
+	info = []
+	t_data = []
+	x_data = []
+	y_data = []
+	for line in info0:
 	
-	print data
-	
-	table.append((name[1],name[3]))
-	
-table.sort()
-	
-print ('\n'.join('%s %s' % x for x in table))
+		info.append(line.split())
+			
+	info.pop(0)
+	info.remove(['#', '[second]', '[meter]', '[meter]'])
+	for line in info:
+		
+		t_data.append(float(line[0]))
+		x_data.append(float(line[1]))
+		y_data.append(float(line[2]))
+		
+	x_reg = numpy.polyfit(numpy.array(t_data), numpy.array(x_data), 1.0)
+	y_reg = numpy.polyfit(numpy.array(t_data), numpy.array(y_data), 2.0)
+	# print (info)
 
+	table.append((float(name[1]),float(name[3]))+tuple(x_reg)+tuple(y_reg))
 	
+# print ('\n'.join('%f %f %f %f %f %f %f' % x for x in table))
+
+for line in table:
+
+	print (line)
+	print '\n'
+
+		
